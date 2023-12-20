0959a769dd9c472cc99707bd4479435defed9943
Tim Huitt <timhuitt@gmail.com>
2023-12-20 12:21:44 -0700

    update header

--- src/Components/Header/Header.jsx
+++ src/Components/Header/Header.jsx
@@ -1,9 +1,15 @@
+
+
 import './Header.css'
 
 export default function Header(){
-    return (
+  return (
+    <div className='Header'>
       <header>
-
+        <div className="header-left">
+          <h1>Auto Component</h1>
+        </div>
       </header>
-    )
+    </div>
+  )
 }
