aede56b3b2ebb386ff7815ab80cb9b39e3e9f984
Tim Huitt <timhuitt@gmail.com>
2023-12-21 23:25:31 -0700

    update link

--- src/Components/Header/index.jsx
+++ src/Components/Header/index.jsx
@@ -18,7 +18,7 @@ export default function Header() {
           </Link>
         </div>
         <div id="header-right" onClick={toggleMenu}>
-          <Link to="https://github.com/TimHuitt/client-auto-component" target="_blank" rel="noopener noreferrer">
+          <Link to="https://github.com/TimHuitt/auto-component" target="_blank" rel="noopener noreferrer">
             GitHub
           </Link>
           <Link to="https://www.npmjs.com/package/auto-component" target="_blank" rel="noopener noreferrer">
